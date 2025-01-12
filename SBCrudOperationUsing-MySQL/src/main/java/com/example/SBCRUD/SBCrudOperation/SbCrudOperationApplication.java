224972f2143bc27e18b503d32a4c614521409314
archanapathak <archana.pathak85@gmail.com>
2025-01-12 13:21:57 +0530

    actuator swagger updates

--- SBCrudOperationUsing-MySQL/src/main/java/com/example/SBCRUD/SBCrudOperation/SbCrudOperationApplication.java
+++ SBCrudOperationUsing-MySQL/src/main/java/com/example/SBCRUD/SBCrudOperation/SbCrudOperationApplication.java
@@ -1,10 +1,14 @@
 package com.example.SBCRUD.SBCrudOperation;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title="Books NEW API",version = "1.1",description = "books api documentation using swagger"))
 public class SbCrudOperationApplication {
 
 	public static void main(String[] args) {
