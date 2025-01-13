cd5ec388f6ba72b21900fe4a980c0870e286c8a5
archanapathak <archana.pathak85@gmail.com>
2025-01-13 10:19:46 +0530

    adding project for calling external api

---
+++ Calling-ExternalApi/src/main/java/com/example/Calling_ExternalApi/CallingExternalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Calling_ExternalApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallingExternalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallingExternalApiApplication.class, args);
+	}
+
+}
---
+++ Calling-ExternalApi/src/test/java/com/example/Calling_ExternalApi/CallingExternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Calling_ExternalApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallingExternalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
