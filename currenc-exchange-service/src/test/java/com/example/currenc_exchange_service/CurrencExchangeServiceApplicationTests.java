d2a31be6f88d94943a6ec5bffacdd9c8f81316ba
archanapathak <archana.pathak85@gmail.com>
2025-02-11 17:36:20 +0530

    new CurrencyExchange API

---
+++ currenc-exchange-service/src/test/java/com/example/currenc_exchange_service/CurrencExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currenc_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
