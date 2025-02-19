8e1130b45fbb61e668d7fa7935510617be53542c
archanapathak <archana.pathak85@gmail.com>
2025-02-19 12:39:46 +0530

    adding currency-exchange microservices

--- Microservices-data-feignclient-and-withloadbalancing/currency-conversion-service/src/main/java/com/example/currency_conversion_service/CurrencyConversionController.java
+++ Microservices-data-feignclient-and-withloadbalancing/currency-conversion-service/src/main/java/com/example/currency_conversion_service/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package com.example.currency_conversion_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 @RestController
 public class CurrencyConversionController {
+    @Autowired
+    private CurrencyExchangeProxy proxy;
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from,@PathVariable String to,
@@ -30,5 +33,17 @@ public class CurrencyConversionController {
                 from, to, quantity,currencyConversion.getConversionMultiple(),quantity.multiply(currencyConversion.getConversionMultiple()),
                 currencyConversion.getEnvironment());
       //  return new CurrencyConversion(1000L, from, to, quantity,  BigDecimal.ONE, BigDecimal.ONE, "");
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,@PathVariable String to,
+            @PathVariable BigDecimal quantity)
+    {
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,currencyConversion.getConversionMultiple(),quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment());
+        //  return new CurrencyConversion(1000L, from, to, quantity,  BigDecimal.ONE, BigDecimal.ONE, "");
     }
 }
