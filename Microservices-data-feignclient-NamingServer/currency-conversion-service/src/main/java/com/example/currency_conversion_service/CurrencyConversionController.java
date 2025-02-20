8a7c659e2d27bc94fca0dcce63d46b40ec9df71c
archanapathak <archana.pathak85@gmail.com>
2025-02-20 11:06:48 +0530

    Adding Project with Service discovery naming server implementation

---
+++ Microservices-data-feignclient-NamingServer/currency-conversion-service/src/main/java/com/example/currency_conversion_service/CurrencyConversionController.java
@@ -0,0 +1,49 @@
+package com.example.currency_conversion_service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+@RestController
+public class CurrencyConversionController {
+    @Autowired
+    private CurrencyExchangeProxy proxy;
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable String from,@PathVariable String to,
+            @PathVariable BigDecimal quantity)
+    {
+        HashMap<String,String> urivariables = new HashMap<>();
+        urivariables.put("from",from);
+        urivariables.put("to",to);
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8001/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversion.class, urivariables);
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,currencyConversion.getConversionMultiple(),quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment());
+      //  return new CurrencyConversion(1000L, from, to, quantity,  BigDecimal.ONE, BigDecimal.ONE, "");
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,@PathVariable String to,
+            @PathVariable BigDecimal quantity)
+    {
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,currencyConversion.getConversionMultiple(),quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment()+" "+"feign");
+        //  return new CurrencyConversion(1000L, from, to, quantity,  BigDecimal.ONE, BigDecimal.ONE, "");
+    }
+}
